2087e52bc49e2b8af381984a7f5ce49dada2daec
James Tauber <jtauber@jtauber.com>
2014-01-14 08:04:14 +0800

    added classifiers for PyPI and released

--- setup.py
+++ setup.py
@@ -2,11 +2,17 @@ from setuptools import setup
 
 setup(
     name = "greek-accentuation",
-    version = "0.4",
+    version = "0.4.1",
     description = "Python 3 library for accenting (and analyzing the accentuation of) Ancient Greek words",
     license = "MIT",
     url = "http://github.com/jtauber/greek-accentuation",
     author = "James Tauber",
     author_email = "jtauber@jtauber.com",
     py_modules = ["characters", "syllabify", "accentuation"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.3",
+        "Topic :: Text Processing :: Linguistic",
+    ],
 )
