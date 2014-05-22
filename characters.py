6b87ad39cd65439d3c454c10f7596c2dbc19cf1e
James Tauber <jtauber@jtauber.com>
2014-05-21 22:17:57 -0400

    refactored characters module to reduce duplication

--- characters.py
+++ characters.py
@@ -5,71 +5,71 @@ def base(ch):
     return unicodedata.normalize("NFD", ch)[0]
 
 
+def extract_diacritic(*diacritics):
+    """
+    Given a collection of Unicode diacritics, return a function that takes a
+    Unicode character and returns the member of the collection the character
+    has (or None).
+    """
+    def _(ch):
+        decomposed_form = unicodedata.normalize("NFD", ch)
+        for diacritic in diacritics:
+            if diacritic in decomposed_form:
+                return diacritic
+    return _
+
+
+def add_diacritic(base, diacritic):
+    """
+    Add the given diacritic to the given base character.
+    """
+    return unicodedata.normalize("NFC", base + diacritic)
+
+
+def remove_diacritic(*diacritics):
+    """
+    Given a collection of Unicode diacritics, return a function that takes a
+    string and returns the string without those diacritics.
+    """
+    def _(text):
+        return unicodedata.normalize("NFC", "".join(
+            ch
+            for ch in unicodedata.normalize("NFD", text)
+            if ch not in diacritics)
+        )
+    return _
+
+
 PSILI = SMOOTH = "\u0313"
 DASIA = ROUGH = "\u0314"
 
-
-def breathing(ch):
-    decomposed_form = unicodedata.normalize("NFD", ch)
-    for breathing_character in [SMOOTH, ROUGH]:
-        if breathing_character in decomposed_form:
-            return breathing_character
+breathing = extract_diacritic(SMOOTH, ROUGH)
 
 
 OXIA = ACUTE = "\u0301"
 VARIA = GRAVE = "\u0300"
 PERISPOMENI = CIRCUMFLEX = "\u0342"
 
-
-def accent(ch):
-    decomposed_form = unicodedata.normalize("NFD", ch)
-    for accent_character in [ACUTE, GRAVE, CIRCUMFLEX]:
-        if accent_character in decomposed_form:
-            return accent_character
+accent = extract_diacritic(ACUTE, GRAVE, CIRCUMFLEX)
 
 
 DIAERESIS = "\u0308"
 
-
-def diaeresis(ch):
-    decomposed_form = unicodedata.normalize("NFD", ch)
-    for accent_character in [DIAERESIS]:
-        if accent_character in decomposed_form:
-            return accent_character
+diaeresis = extract_diacritic(DIAERESIS)
 
 
 YPOGEGRAMMENI = IOTA_SUBSCRIPT = "\u0345"
 
-
-def iota_subscript(ch):
-    decomposed_form = unicodedata.normalize("NFD", ch)
-    for accent_character in [IOTA_SUBSCRIPT]:
-        if accent_character in decomposed_form:
-            return accent_character
+iota_subscript = extract_diacritic(IOTA_SUBSCRIPT)
 ypogegrammeni = iota_subscript
 
 
 SHORT = "\u0306"
 LONG = "\u0304"
 
+length = extract_diacritic(SHORT, LONG)
 
-def length(ch):
-    decomposed_form = unicodedata.normalize("NFD", ch)
-    for diacritic in [SHORT, LONG]:
-        if diacritic in decomposed_form:
-            return diacritic
-
-
-def strip_length(t):
-    return unicodedata.normalize("NFC", "".join(
-        ch
-        for ch in unicodedata.normalize("NFD", t)
-        if ch not in [SHORT, LONG])
-    )
-
-
-def add_diacritic(base, diacritic):
-    return unicodedata.normalize("NFC", base + diacritic)
+strip_length = remove_diacritic(SHORT, LONG)
 
 
 if __name__ == "__main__":
@@ -87,6 +87,7 @@ if __name__ == "__main__":
     assert length("ῠ") == SHORT
     assert length("ῡ") == LONG
     assert length("υ") is None  # i.e. unknown
+    assert strip_length("ῡ") == "υ"
 
     assert add_diacritic("υ", DIAERESIS) == "ϋ"
     assert add_diacritic("α", YPOGEGRAMMENI) == "ᾳ"
